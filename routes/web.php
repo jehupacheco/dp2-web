d10ee944a94bad460e0131061de1d832a39a6f1d
JoseCh09 <jose.chunga@pucp.pe>
2017-10-20 04:49:11 -0500

    Estacionamiento
    Se agrega la vista de estacionamiento

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::get('/ubicaciones/buscar_usuario','LocationController@search_user');
 Route::get('/ubicaciones/buscar/usuarios','LocationController@users_result');
 
 Route::get('/ubicaciones/usuario/1/mapa','LocationController@mostrar_mapa');
+
+Route::get('/estacionamiento','ParkingController@index');
