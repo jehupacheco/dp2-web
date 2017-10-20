813c3c61cdb22080864378b655bacad783bfc5ec
cesarolivera <cesar.olivera@pucp.pe>
2017-10-20 15:25:21 -0500

    se agrego la pantalla de los autos

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::get('/ubicaciones/buscar_usuario','LocationController@search_user');
 Route::get('/ubicaciones/buscar/usuarios','LocationController@users_result');
 
 Route::get('/ubicaciones/usuario/1/mapa','LocationController@mostrar_mapa');
+
+Route::get('/autos/tipo/{tipo_id}/lista','AutoController@mostrar_lista_tipo');
