064c1fbbe9d15ede0009293eb0b84b62ff2ae866
cesarolivera <cesar.olivera@pucp.pe>
2017-11-22 15:53:20 -0500

    Aplicando permisos de roles en las vistas blades - Laravel Permissions

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -27,25 +27,37 @@
             <div class="menu_section">
                 <h3>Menú</h3>
                 <ul class="nav side-menu">
+
+                    @can('Dashboard')
                     <li>
                         <a href="{{url('/')}}">
                             <i class="fa fa-dashboard"></i>
                             Dashboard
                         </a>
                     </li>
+                    @endcan
+                    @can('Organizaciones')
                     <li>
                         <a href="{{route('organizations.index')}}">
                             <i class="fa fa-building"></i>
                             Organizaciones
                         </a>
                     </li>
+                    @endcan
+                    @can('Clientes')
                     <li>
                         <a href="{{url('/clientes')}}">
                             <i class="fa fa-user"></i>
                             Clientes
                         </a>
                     </li>
-
+                    @endcan
+                    @if(auth()->user()->can('Vehículos para pacientes de Cardiopatía') ||
+                        auth()->user()->can('Vehículos para la Jardinería') ||
+                        auth()->user()->can('Vehículos para Ventas') ||
+                        auth()->user()->can('Vehículos Eco-amigables') ||
+                        auth()->user()->can('Vehículos para Trasporte Urbano 1') ||
+                        auth()->user()->can('Vehículos para Trasporte Urbano 2'))
                     <li>
                         <a>
                             <i class="fa fa-bus"></i> Vehiculos 
@@ -53,10 +65,29 @@
                         </a>
                         <ul class="nav child_menu">
                             @foreach($all_organizations as $org)
+                                @if($org->name == "Cardiopatia" && auth()->user()->can('Vehículos para pacientes de Cardiopatía'))
                                 <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+                                @if($org->name == "Jardineria" && auth()->user()->can('Vehículos para la Jardinería'))
+                                <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+                                @if($org->name == "Ventas" && auth()->user()->can('Vehículos para Ventas'))
+                                <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+                                @if($org->name == "Eco-amigable" && auth()->user()->can('Vehículos Eco-amigables'))
+                                <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+                                @if($org->name == "Transporte Urbano 1" && auth()->user()->can('Vehículos para Trasporte Urbano 1'))
+                                <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+                                @if($org->name == "Transporte Urbano 1" && auth()->user()->can('Vehículos para Trasporte Urbano 2'))
+                                <li><a href="{{url('/vehiculos/'.$org->id.'/lista')}}">Vehículo {{$org->name}}</a></li>
+                                @endif
+
                             @endforeach
                         </ul>
                     </li>
+                    @endif
                     <li>
                         <!-- <a href="{{url('/alquiler/index')}}"> -->
                         <a href="{{url('/alquileres/index')}}">
@@ -64,14 +95,28 @@
                             Alquileres
                         </a>
                     </li>
+                    @if(auth()->user()->can('Reportes de Recorridos') ||
+                        auth()->user()->can('Reportes de Clientes') ||
+                        auth()->user()->can('Reportes de Sensores') ||
+                        auth()->user()->can('Reportes de Historial de Alertas'))
                     <li><a><i class="fa fa-bar-chart"></i> Reportes <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
+                            @if(auth()->user()->can('Reportes de Recorridos'))
                             <li><a href="{{url('/reportes/clienteXvehiculo')}}">Reporte de Recorridos</a></li>
+                            @endif
+                            @if(auth()->user()->can('Reportes de Clientes'))
                             <li><a href="{{url('/reportes/filtrosReportes')}}">Reporte de Clientes</a></li>
+                            @endif
+                            @if(auth()->user()->can('Reportes de Sensores'))
                             <li><a href="{{url('/reportes/sensores')}}">Reporte de Sensores</a></li>
+                            @endif
+                            @if(auth()->user()->can('Reportes de Historial de Alertas'))
                             <li><a href="#">Reporte de Historial de Alertas</a></li>
+                            @endif
                         </ul>
                     </li>
+                    @endif
+                    @if(auth()->user()->can('Seguridad'))
                     <li><a><i class="fa fa-cog"></i> Seguridad <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                             <li><a href="{{url('/usuarios/nuevo')}}">Crear usuario</a></li>
@@ -79,17 +124,21 @@
                             <li><a href="{{url('/roles')}}">Roles</a></li>
                         </ul>
                     </li>
+                    @endif
+                    @if(auth()->user()->can('Configuración'))
                     <li>
                         <a href="{{url('/vehiculos/Configuracion')}}">
                             <i class="fa fa-asterisk"></i>Configuracion
                         </a>
                     </li>
+                    @endif
+                    @if(auth()->user()->can('Estacionamiento'))
                     <li>
                         <a href="{{url('/estacionamiento')}}">
                             <i class="fa fa-car"></i>Estacionamiento
                         </a>
                     </li>
-                    
+                    @endif
                     {{--  <li>
                         <a href="{{url('/autos/tipo/6/lista')}}">
                             <i class="fa fa-car"></i>Vehículo transporte urbano
