8478b9bfbcc5b64e2f1c2d111be0a86b4562a1ba
cesarolivera <cesar.olivera@pucp.pe>
2017-12-04 11:58:22 -0500

    Volviendo a guardar imagen y arreglando la vista de roles que fue cambiada en el ultimo PR

--- resources/views/Seguridad/roles/index.blade.php
+++ resources/views/Seguridad/roles/index.blade.php
@@ -23,7 +23,7 @@
       <div class="clearfix"></div>
     </div>
     <div class="row">
-        <div class="col-md-10 col-sm-10 col-xs-10">
+        <div class="col-md-12 col-sm-12 col-xs-12">
           <div class="x_panel">
               <div class="x_title">
                 <h2>Roles <small>Lista de Roles de Usuarios</small></h2>
